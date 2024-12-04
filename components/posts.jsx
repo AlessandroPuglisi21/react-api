d51b1eb310a53b6dbae1d41f3a81ad9386c2537e
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-12-04 15:55:46 +0100

    First Commit

---
+++ components/posts.jsx
@@ -0,0 +1,44 @@
+const posts = [
+	{
+		id: 1,
+		title: 'Titolo del Post',
+		image: 'https://picsum.photos/id/237/200/300' /* compila questo campo */,
+		content:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.',
+		tags: ['html', 'css'],
+		published: true,
+		categories: 'Programmazione'
+	},
+	{
+		id: 2,
+		title: 'Titolo del Post',
+		image: 'https://picsum.photos/seed/picsum/200/300' /* compila questo campo */,
+		content:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.',
+		tags: ['js', 'css'],
+		published: true,
+		categories: 'Programmazione'
+	},
+	{
+		id: 3,
+		title: 'Titolo del Post',
+		image: 'https://picsum.photos/200/300?grayscale' /* compila questo campo */,
+		content:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.',
+		tags: ['js', 'php'],
+		published: true,
+		categories: 'Programmazione'
+	},
+	{
+		id: 4,
+		title: 'Titolo del Post',
+		image: 'https://picsum.photos/200/300/?blur' /* compila questo campo */,
+		content:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.',
+		tags: ['html'],
+		published: false,
+		categories: 'Programmazione'
+	},
+]
+
+export default posts
